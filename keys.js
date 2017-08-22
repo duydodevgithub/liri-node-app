692f0ff0780aa45931359c8085302c39ff3c0993
DuyDo <duydo.work@gmail.com>
2017-08-22 00:14:41 -0500

    finish assigment

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'iuTmFCkdURCJutT6hiOa7vxdp',
+  consumer_secret: 'tzDxJdwPElwpxd1t3Z0F5Gd3FA4bPN4nKwHKWjSpiVJWdKnJFd',
+  access_token_key: '891186373008535553-VA1SaFC0dAowxdLcyKNM6Yui0PbNvEC',
+  access_token_secret: 'mwDT2EjYS99CF5xus3aX5W8lx2rDLet5eyVRpHkhxHOHy'
+}
+
+exports.spotifyKeys = {
+	id: "bf9cca38951243fc9dcae67678019223",
+  secret: "5a39f064a7ec4fcfbf3d04936d3d2731"
+}
